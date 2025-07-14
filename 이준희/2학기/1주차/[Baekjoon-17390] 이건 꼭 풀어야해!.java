e6420fa1da0e294d4bfa40b4a4b6d952d7c669d3
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-07-14 13:43:11 +0900

    [Baekjoon-17390] 이건 꼭 풀어야해!

---
+++ 이준희/2학기/1주차/[Baekjoon-17390] 이건 꼭 풀어야해!.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static Queue<int[]> list = new ArrayDeque<>();
+    static int n, q;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] sum = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        sum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            sb.append(sum[to] - sum[from] + arr[from] + "\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+
+}
