d7df6c4a5bcefc96f38d9012a21552b4b5396857
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-07-16 15:29:35 +0900

    [Baekjoon-17390] 이건 꼭 풀어야해!

--- 이준희/2학기/1주차/[Baekjoon-17390] 이건 꼭 풀어야해!.java
+++ 이준희/2학기/1주차/[Baekjoon-17390] 이건 꼭 풀어야해!.java
@@ -25,14 +25,14 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(arr); // 비내림차순으로 정렬
 
         sum[0] = arr[0];
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) { // 구간합 구해놓기
             sum[i] = sum[i - 1] + arr[i];
         }
 
-        for (int i = 0; i < q; i++) {
+        for (int i = 0; i < q; i++) { // 입력에 따라 출력하기
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken()) - 1;
             int to = Integer.parseInt(st.nextToken()) - 1;
